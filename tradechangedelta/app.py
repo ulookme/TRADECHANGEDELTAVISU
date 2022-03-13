7e23f2481097ee537f5b31a260fb3418c081ab64
charles <48330531+ulookme@users.noreply.github.com>
2022-03-13 13:56:23 +0100

    Update app.py

--- tradechangedelta/app.py
+++ tradechangedelta/app.py
@@ -123,7 +123,7 @@ def refresher(seconds):
             f.write(f'# {randint(0, 10000)}')
         time.sleep(seconds)
 
-refresher(1)
+refresher(4)
 #st.sidebar.title("Control Panel")
 #right_col = st.beta_columns(1)
 
